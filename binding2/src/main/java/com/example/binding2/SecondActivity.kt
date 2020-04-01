d94bb61069c332fcfdb92680a5a9588d54b519bc
OmarGuerrero-Globant <omar.reyes@globant.com>
2020-04-01 09:31:22 -0600

    Resolving requested changes
    From code review the requested change of avoiding the use of double bang by the use of the lateinit modifier.

--- binding2/src/main/java/com/example/binding2/SecondActivity.kt
+++ binding2/src/main/java/com/example/binding2/SecondActivity.kt
@@ -8,7 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 
 
 class SecondActivity : AppCompatActivity() {
-    private var recyclerView: RecyclerView? = null
+    private lateinit var recyclerView: RecyclerView
     private var mAdapter: RecyclerView.Adapter<*>? = null
     private var layoutManager: RecyclerView.LayoutManager? = null
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,14 +16,14 @@ class SecondActivity : AppCompatActivity() {
         setContentView(R.layout.activity_second)
         recyclerView = findViewById<View>(R.id.my_recycler_view) as RecyclerView
 
-        recyclerView!!.setHasFixedSize(true)
+        recyclerView.setHasFixedSize(true)
 
         layoutManager = LinearLayoutManager(this)
-        recyclerView!!.layoutManager = layoutManager
+        recyclerView.layoutManager = layoutManager
         val items: List<TemperatureData> =
             mutableListOf(TemperatureData("Hamburg", "5"), TemperatureData("Berlin", "6"))
 
         mAdapter = MyAdapter(items)
-        recyclerView!!.adapter = mAdapter
+        recyclerView.adapter = mAdapter
     }
 }
