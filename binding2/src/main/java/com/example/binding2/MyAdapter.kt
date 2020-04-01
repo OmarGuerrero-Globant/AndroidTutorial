968df5c0ea6602d43cb9a79a757a37e0a6604480
OmarGuerrero-Globant <omar.reyes@globant.com>
2020-03-31 19:40:47 -0600

    Second Activity feature
    The second activity was added for implementation on the second buttton "start second activity".

--- binding2/src/main/java/com/example/binding2/MyAdapter.kt
+++ binding2/src/main/java/com/example/binding2/MyAdapter.kt
@@ -1,18 +1,47 @@
 package com.example.binding2
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+
 class MyAdapter
-    (var data: List<TemperatureData>) :
-    MyBaseAdapter() {
-    override fun getDataAtPosition(position: Int): Any {
-        return data[position]
+    (private val data: List<TemperatureData>) :
+    RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+    inner class MyViewHolder(
+        private val binding: ViewDataBinding
+    ) : ViewHolder(binding.root) {
+        fun bind(obj: Any?) {
+            binding.setVariable(BR.obj, obj)
+            binding.executePendingBindings()
+        }
     }
 
-    override fun getLayoutIdForType(viewType: Int): Int {
-        return R.layout.row_layout
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MyViewHolder {
+
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = DataBindingUtil.inflate<ViewDataBinding>(
+            layoutInflater,
+            R.layout.row_layout,
+            parent,
+            false
+        )
+
+        return MyViewHolder(binding)
     }
 
-    override fun getItemCount(): Int {
-        return data.size
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val temperatureData = data[position]
+        holder.bind(temperatureData)
     }
+
+    override fun getItemCount(): Int = data.size
 
 }
