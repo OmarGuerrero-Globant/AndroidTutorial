d8d39458d8124404ac82a4275833be9ea759a128
Omar Guerrero <omar.reyes@globant.com>
2020-04-02 14:35:47 -0600

    Landscape Portrait implementation
    The RssfeedActivity should show two fragments (two panes) if started in landscape mode. If it is started in portrait mode only one fragment should be shown. In this exercise you adjust your application to support this.

--- rssreader/src/main/java/com/example/rssreader/RssfeedActivity.kt
+++ rssreader/src/main/java/com/example/rssreader/RssfeedActivity.kt
@@ -2,22 +2,45 @@ package com.example.rssreader
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 
 class RssfeedActivity : AppCompatActivity(),
     MyListFragment.OnItemSelectedListener {
+
+    private val fm: FragmentManager = supportFragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rssfeed)
+        if(resources.getBoolean(R.bool.twoPaneMode)){return}
+        if(savedInstanceState != null){
+            fm.executePendingTransactions()
+            val fragmentById: Fragment? = fm.findFragmentById(R.id.fragment_container)
+            if (fragmentById !=null){
+                fm.beginTransaction()
+                    .remove(fragmentById).hashCode()
+            }
+        }
+        val listFragment: MyListFragment = MyListFragment()
+        fm.beginTransaction().replace(R.id.fragment_container, listFragment).commit()
     }
 
     override fun onRssItemSelected(text: String?) {
-
-        val fm: FragmentManager = supportFragmentManager
-        (fm
-            .findFragmentById(R.id.detailFragment) as DetailFragment).apply {
-            this.setText(text)
+        if (resources.getBoolean(R.bool.twoPaneMode)){
+            val fragment : DetailFragment = fm.findFragmentById(R.id.detailFragment) as DetailFragment
+            fragment.setText(text)
+        }else{
+            val newFragment : DetailFragment = DetailFragment()
+            val args : Bundle = Bundle()
+            args.putString(DetailFragment.EXTRA_TEXT, text)
+            newFragment.arguments = args
+            val transaction: FragmentTransaction = fm.beginTransaction()
+            transaction.replace(R.id.fragment_container, newFragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
         }
     }
 }
