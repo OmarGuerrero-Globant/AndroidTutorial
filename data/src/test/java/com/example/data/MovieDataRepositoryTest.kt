7dd565779176005115fdf166ca051394368b624b
Omar Guerrero <omar.reyes@globant.com>
2020-04-28 14:52:23 -0500

    MovieDataRepositoryTest implementation
    success testcases for the MovieDataRepositoryTest.

--- data/src/test/java/com/example/data/MovieDataRepositoryTest.kt
+++ data/src/test/java/com/example/data/MovieDataRepositoryTest.kt
@@ -1,5 +1,6 @@
 package com.example.data
 
+import com.example.data.remote.responses.GetMovieResponse
 import com.example.data.remote.services.MoviesServices
 import io.reactivex.Single
 import org.junit.Before
@@ -9,13 +10,17 @@ import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.junit.MockitoJUnitRunner
 import repositories.MovieDataRepository
+import retrofit2.Response
 import kotlin.properties.Delegates
 
 @RunWith(MockitoJUnitRunner.StrictStubs::class)
 class MovieDataRepositoryTest {
 
-    private var mockPage : Int by Delegates.notNull<Int>()
+    private var mockPage : Int by Delegates.notNull()
+    private lateinit var mockId : String
     private lateinit var  message : String
+    private lateinit var movieMock : GetMovieResponse
+    private lateinit var moviesMock : List<GetMovieResponse>
 
     @Mock
     private lateinit var moviesServices: MoviesServices
@@ -27,9 +32,40 @@ class MovieDataRepositoryTest {
     @Before
     fun setUp(){
         mockPage = 1
+        mockId = "1"
         message = "Not Found"
+        movieMock = GetMovieResponse("1", "test", "overview","imageTest")
+        moviesMock = listOf(
+            GetMovieResponse("1", "test", "overview","imageTest"),
+            GetMovieResponse("2", "test", "overview","imageTest")
+        )
     }
 
+    @Test
+    fun validateGetMovieByIdSuccess(){
+        Mockito.`when`(moviesServices.getMovieById(1)).thenReturn(Single.just(Response.success(movieMock)))
+        movieDataRepository.getMovieById("1")
+            .test()
+            .assertComplete()
+            .assertNoErrors()
+            .assertValue { movieDto ->
+                movieDto.id == "1"
+            }
+    }
+
+    @Test
+    fun validateGetMoviesSuccess(){
+        Mockito.`when`(moviesServices.getMovies(1)).
+        thenReturn(Single.just(Response.success(moviesMock)))
+
+        movieDataRepository.getMovies(mockPage)
+            .test()
+            .assertComplete()
+            .assertNoErrors()
+            .assertValue { movies ->
+                movies.size == 2
+            }
+    }
 
     @Test
     fun validateGetMovieError(){
@@ -45,7 +81,7 @@ class MovieDataRepositoryTest {
     @Test
     fun validateGetMovieByIdError(){
         Mockito.`when`(moviesServices.getMovieById(1)).thenReturn(Single.error(Throwable(message)))
-        movieDataRepository.getMovieById("1")
+        movieDataRepository.getMovieById(mockId)
             .test()
             .assertError {
                 it.message == message
